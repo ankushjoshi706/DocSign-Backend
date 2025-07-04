23d16868902486556ccb28b0ee3b1cb135f9fc7f
ankushjoshi706 <85740434+ankushjoshi706@users.noreply.github.com>
2025-07-05 03:02:55 +0530

    Update Signature.js

--- models/Signature.js
+++ models/Signature.js
@@ -1,4 +1,5 @@
-const mongoose = require("mongoose");
+// models/Signature.js
+import mongoose from "mongoose";
 
 const signatureSchema = new mongoose.Schema({
   documentId: { type: mongoose.Schema.Types.ObjectId, ref: "Document" },
@@ -8,4 +9,5 @@ const signatureSchema = new mongoose.Schema({
   page: Number,
 });
 
-module.exports = mongoose.model("Signature", signatureSchema);
+const Signature = mongoose.model("Signature", signatureSchema);
+export default Signature; // ✅ ES Module default export
